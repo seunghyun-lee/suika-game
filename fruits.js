be50958d7682d899879635edb08f886211e2f8a6
Seunghyun Lee <seunghyunleekr@gmail.com>
2024-09-04 11:05:53 +0900

    initial commit

---
+++ fruits.js
@@ -0,0 +1,57 @@
+export const FRUITS = [
+    {
+      name: "cherry",
+      radius: 40 / 2,
+      color: "#F20306",
+    },
+    {
+      name: "strawberry",
+      radius: 50 / 2,
+      color: "#FF624C",
+    },
+    {
+      name: "grape",
+      radius: 72 / 2,
+      color: "#A969FF",
+    },
+    {
+      name: "kumkwat",
+      radius: 85 / 2,
+      color: "#FFAF02",
+    },
+    {
+      name: "orange",
+      radius: 106 / 2,
+      color: "#FC8611",
+    },
+    {
+      name: "apple",
+      radius: 140 / 2,
+      color: "#F41615",
+    },
+    {
+      name: "pear",
+      radius: 160 / 2,
+      color: "#FDF176",
+    },
+    {
+      name: "peach",
+      radius: 196 / 2,
+      color: "#FEB6AC",
+    },
+    {
+      name: "pineapple",
+      radius: 220 / 2,
+      color: "#F7E608",
+    },
+    {
+      name: "melon",
+      radius: 270 / 2,
+      color: "#89CE13",
+    },
+    {
+      name: "watermelon",
+      radius: 300 / 2,
+      color: "#26AA1E",
+    },
+  ];
