ac9da4b92bbb67c600c856e370c8a995d1ebda50
Seunghyun Lee <seunghyunleekr@gmail.com>
2024-11-15 22:39:35 +0900

    sitemap, robots.txt 추가

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  base: '/',
-  build: {
-    outDir: 'dist',
-  },
+    base: '/',
+    build: {
+        outDir: 'dist',
+    },
+    plugins: [
+        sitemap({
+            hostname: 'https://suika-game-eight.vercel.app',
+            routes: [
+                '/',
+            ],
+        }),
+  ],
 });
